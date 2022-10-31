bb85db486edd0a3f89350325f52d5df714aea714
Eduardo-marin-nino <angel_marinni@fet.edu.co>
2022-10-31 16:25:34 -0500

    change api key

--- lib/core/config/constants.dart
+++ lib/core/config/constants.dart
@@ -3,4 +3,4 @@ const String urlGooglePlaces = "https://maps.googleapis.com";
 const String urlPlacesDetails = "maps/api/place/details/json";
 const String urlPlacesAutocomplete = "maps/api/place/autocomplete/json";
 
-const String apiKeyGMapsAndroid = "AIzaSyAKwbon3C0ScspsV7eYqRWmsSAAHnrXf4A";
+const String apiKeyGMapsAndroid = "AIzaSyBoTmYwPYfM8S6nrTJ_3NO_bSKkP9D6ZJ8";
